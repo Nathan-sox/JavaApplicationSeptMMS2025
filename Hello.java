6f06689a865ad73d7f3a430ce22f0314d52164b1
Nathan-sox <nathokere2009@gmail.com>
2026-05-06 16:21:34 +0100

    Class 1

---
+++ Hello.java
@@ -0,0 +1,11 @@
+public class Hello{
+	public static void main(String[] args) {
+		System.out.println("Hello everyone, I am learning Java");
+		System.out.print("Oh wow, Learning Java is fun ");
+		System.out.print("No not really, This is a misery ");
+		System.out.printf("Java was created in in %d on %d March %n", 1992, 1);
+		System.out.printf("Java was made free for %c%.2f dollar %n", '$', 0.0000);
+		System.out.printf("Do you love Java %c %b ",'?',true);
+		System.out.printf("Yes learning %s is fun", "Java");
+		}
+}
