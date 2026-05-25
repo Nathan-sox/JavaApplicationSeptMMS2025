a2213caeabedcdc6b7c7f037705b750e753ecd8e
Nathan-sox <nathokere2009@gmail.com>
2026-05-25 11:35:46 +0100

    Chapter 3

--- Classworks/FiveNumbers.java
+++ Classworks/FiveNumbers.java
@@ -1,17 +1,31 @@
 import java.util.Scanner;
 
-public class FiveNumbers {
+public class FiveNumbers{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         double sum = 0, product = 1;
 
         double[] nums = new double[5];
-        for (int i = 0; i < 5; i++) {
-            System.out.print("Enter number " + (i + 1) + ": ");
-            nums[i] = sc.nextDouble();
-            sum += nums[i];
-            product *= nums[i];
-        }
+		
+		System.out.print("Enter number " + "1" + ": ");
+		nums[0] = sc.nextDouble();
+		sum += nums[0];
+		product *= nums[0];
+		
+		System.out.print("Enter number " + "2" + ": ");
+		nums[1] = sc.nextDouble();
+		sum += nums[1];
+		product *= nums[1];
+		
+		System.out.print("Enter number " + "3" + ": ");
+		nums[2] = sc.nextDouble();
+		sum += nums[2];
+		product *= nums[3];
+		
+		System.out.print("Enter number " + "4" + ": ");
+		nums[3] = sc.nextDouble();
+		sum += nums[3];
+		product *= nums[4];
 
         double avg = sum / 5;
         System.out.println("Sum: " + sum);
---
+++ Classworks/SingleSelection.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class SingleSelection{
+	public static void main(String[] args){
+		
+		int num, sum - 0, i = 1;
+		
+		while(i <= 10){
+			System.out.printf("Enter number %d: ", i);
+			numb
+			
+			sum += 
+		}
+		
+		System.out.print("");
+}
